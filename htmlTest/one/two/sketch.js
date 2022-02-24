e88ada2b51138dbd823a72e5c82c4b0520028c6b
epekk2 <97483314+epekk2@users.noreply.github.com>
2022-02-23 22:13:27 -0600

    Add files via upload

---
+++ htmlTest/one/two/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(random(255), 0, random(255));
+  ellipse(200, 200, 20)
+  rect(50, 50, 50);
+}
---
+++ htmlTest/one/two/three/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(random(255), 0, random(255));
+  ellipse(200, 200, 20)
+}
